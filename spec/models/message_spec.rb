972f57da88e178e850fa83a336f7e9f54587096f
Daniel Murphy <danielmichaelmurphy@gmail.com>
2022-01-04 13:03:43 -0500

    Write test for #emphasize

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'emphasizing text' do
+    let(:message) { Message.new }
+    it 'makes the text uppercase and adds an exclamation point' do
+      expect(message.emphasize('hello')).to eq ('HELLO!')
+    end
+  end
 end
